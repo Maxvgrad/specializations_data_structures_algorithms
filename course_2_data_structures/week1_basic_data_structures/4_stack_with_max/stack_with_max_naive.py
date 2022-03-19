ebbffbf998478188f64a7729aa5262fb28793ae8
Maksim Vinogradov <maksim.vinogradov@transferwise.com>
2022-03-19 20:31:06 +0200

    Complete C2W1 T4

--- course_2_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ course_2_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -6,12 +6,12 @@ class StackWithMax:
     def __init__(self):
         self.__stack = []
         # self.sorted_list = SortedLinkedList()
-        self.sorted_list = []
+        self.sorted_list = BinarySearchSortedList()
 
     def Push(self, a):
         self.__stack.append(a)
-        self.sorted_list.append(a)
-        self.sorted_list.sort(reverse=True)
+        self.sorted_list.add(a)
+        # self.sorted_list.sort(reverse=True)
 
     def Pop(self):
         assert (len(self.__stack))
@@ -20,8 +20,70 @@ class StackWithMax:
 
     def Max(self):
         assert (len(self.__stack))
-        return self.sorted_list[0]
+        return self.sorted_list.elements[0]
 
+
+class BinarySearchSortedList:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.elements = []
+
+    def add(self, element):
+        number_of_elements = len(self.elements)
+        if number_of_elements < 5:
+            for i in range(number_of_elements):
+                if element > self.elements[i]:
+                    self.elements.insert(i, element)
+                    return
+            self.elements.append(element)
+            return
+
+        def take(element_, mid_index):
+            if self.elements[mid_index] < element_:
+                if mid_index == 0 or element_ <= self.elements[mid_index - 1]:
+                    return True
+            return False
+
+        index = self.find_index(element, number_of_elements, take)
+        if index == -1:
+            index = number_of_elements
+        self.elements.insert(index, element)
+
+    def remove(self, element):
+        number_of_elements = len(self.elements)
+        if number_of_elements < 5:
+            for i in range(number_of_elements):
+                if element == self.elements[i]:
+                    del self.elements[i]
+                    return
+            return
+
+        def take(element_, mid_index):
+            if self.elements[mid_index] == element_:
+                return True
+
+        index = self.find_index(element, number_of_elements, take)
+        if index == -1:
+            return
+        del self.elements[index]
+
+    def find_index(self, element, number_of_elements, take):
+        left_pointer = 0
+        right_pointer = number_of_elements-1
+
+        while left_pointer <= right_pointer:
+            mid_index = int(round((left_pointer + right_pointer) / 2, 0))
+
+            if take(element, mid_index):
+                return mid_index
+
+            if element > self.elements[mid_index]:
+                right_pointer = mid_index-1
+            else:
+                left_pointer = mid_index+1
+
+        return -1
 
 class SortedLinkedList:
 
--- course_2_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive_test.py
+++ course_2_data_structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive_test.py
@@ -1,4 +1,4 @@
-from stack_with_max_naive import StackWithMax
+from stack_with_max_naive import StackWithMax, BinarySearchSortedList
 import unittest
 
 
@@ -17,7 +17,7 @@ class TestMaxSlidingWindow(unittest.TestCase):
         stack.Pop()
         self.assertEqual(1, stack.Max())
 
-    def test_when_pop_max_should_return_correct_max(self):
+    def test_when_pop_max_should_return_correct_max_2(self):
         stack = StackWithMax()
         stack.Push(1)
         stack.Push(2)
@@ -25,13 +25,6 @@ class TestMaxSlidingWindow(unittest.TestCase):
         stack.Pop()
         self.assertEqual(1, stack.Max())
 
-
-
-    # def test_when_pop_on_empty_stack_should_not_fail(self):
-    #     stack = StackWithMax()
-    #     stack.Pop()
-    #     self.assertTrue(True)
-    #Failed case #10/60: (Wrong answer)
     def test_case_10(self):
         stack = StackWithMax()
         stack.Push(0)
@@ -74,3 +67,130 @@ class TestMaxSlidingWindow(unittest.TestCase):
         self.assertEqual(1, stack.Max())
         stack.Pop()
         self.assertEqual(0, stack.Max())
+
+    # Failed
+    # case  # 11/60: (Wrong answer)
+    def test_case_11(self):
+
+        stack = StackWithMax()
+        stack.Push(15560)
+        self.assertEqual(15560, stack.Max())
+        stack.Push(25104)
+        self.assertEqual(25104, stack.Max())
+        stack.Push(93136)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(19637)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(54647)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(13521)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(57160)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(8087)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(48207)
+        self.assertEqual(93136, stack.Max())
+        stack.Push(53519)
+        self.assertEqual(93136, stack.Max())
+
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(93136, stack.Max())
+        stack.Pop()
+        self.assertEqual(25104, stack.Max())
+        stack.Pop()
+        self.assertEqual(15560, stack.Max())
+
+    def test_binary_sorted_list_when_empty_should_append(self):
+        list = BinarySearchSortedList()
+        list.add(10)
+        self.assertEqual([10], list.elements)
+
+    def test_binary_sorted_list_when_one_element_gt_all_elements_in_list_add_to_zero_index(self):
+        list = BinarySearchSortedList()
+        list.add(10)
+        list.add(20)
+        self.assertEqual([20, 10], list.elements)
+
+    def test_binary_sorted_list_when_small_size_should_add_in_the_end(self):
+        list = BinarySearchSortedList()
+        list.add(20)
+        list.add(10)
+        self.assertEqual([20, 10], list.elements)
+
+    def test_binary_sorted_list_when_small_size_should_add_in_the_middle(self):
+        list = BinarySearchSortedList()
+        list.add(10)
+        list.add(20)
+        list.add(15)
+        self.assertEqual([20, 15, 10], list.elements)
+
+    def test_binary_sorted_list_when_big_size_list_should_add_in_first(self):
+        list = BinarySearchSortedList()
+
+        for i in range(1, 11, 2): #1,3,5,7,9
+            list.add(i)
+        list.add(11)
+        arr = [1, 3, 5, 7, 9, 11]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
+
+
+    def test_binary_sorted_list_when_big_size_list_should_add_in_the_middle(self):
+        list = BinarySearchSortedList()
+
+        for i in range(1, 12, 2): #1,3,5,7,9,11
+            list.add(i)
+        list.add(8)
+        arr = [1, 3, 5, 7, 8, 9, 11]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
+
+    def test_binary_sorted_list_when_big_size_list_should_add_in_the_end(self):
+        list = BinarySearchSortedList()
+        for i in range(1, 12, 2): #1,3,5,7,9,11
+            list.add(i)
+        list.add(-3)
+        arr = [-3, 1, 3, 5, 7, 9, 11]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
+
+
+    def test_binary_sorted_list_should_first_element(self):
+        list = BinarySearchSortedList()
+        for i in range(1, 12, 2): #1,3,5,7,9,11
+            list.add(i)
+        list.remove(11)
+        arr = [1, 3, 5, 7, 9]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
+
+    def test_binary_sorted_list_should_remove_middle_element(self):
+        list = BinarySearchSortedList()
+        for i in range(1, 12, 2): #1,3,5,7,9,11
+            list.add(i)
+        list.remove(5)
+        arr = [1, 3, 7, 9, 11]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
+
+    def test_binary_sorted_list_should_last_element(self):
+        list = BinarySearchSortedList()
+        for i in range(1, 12, 2): #1,3,5,7,9,11
+            list.add(i)
+        list.remove(1)
+        arr = [3, 5, 7, 9, 11]
+        arr.reverse()
+        self.assertEqual(arr, list.elements)
